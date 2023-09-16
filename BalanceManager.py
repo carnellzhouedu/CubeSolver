15f5c4c2bbad846c1c77c9056f498107168fbd78
Carnell Zhou <carnellzhou@MacBook-Pro-8.local>
2023-09-16 00:11:40 -0400

    initial commit

--- BalanceManager.py
+++ BalanceManager.py
@@ -2,4 +2,16 @@ from collections import defaultdict
 
 def get_command_results(commands):
     banks = defaultdict(set) # map bank to users
-    users = defaultdict(int) # map user to balances
+    balances = defaultdict(int) # map user to balances
+
+    for command in commands:
+        t = command[0]
+        if t == "INIT":
+            usr, balance = commands[1], command[2]
+            # set balance
+            balances[usr] = balance
+            for bank in commands[2:]:
+                banks[bank].add(usr)
+
+        
+    
